8bd2b0c81a5756d2eaa3561f1e009d47c62e3381
Nilesh Jain <njain.we@gmail.com>
2020-09-12 11:33:14 +0530

    Working Build

--- server.js
+++ server.js
@@ -24,52 +24,6 @@ const app = express()
 const port = process.env.PORT || 9000;
 
 
-
-// const pusher = new Pusher({
-//   appId: '1069706',
-//   key: '2dc823cb13284cd07f68',
-//   secret: 'aee7cf57ebe848b042c0',
-//   cluster: 'ap2',
-//   encrypted: true
-// });
-
-// const db = mongoose.connection;
-
-// db.once('open',()=>{
-//     console.log("DB is working");
-
-//     const msgCollection = db.collection('messagecontents')
-//     const changeStream = msgCollection.watch()
-
-//     changeStream.on('change',(change)=> {
-//         console.log(change);
-
-//         if(change.operationType === 'insert'){
-//             const messageDetails = change.fullDocument;
-//             pusher.trigger('messages','inserted',
-//             {
-//                 name: messageDetails.name,
-//                 message: messageDetails.message,
-//                 timestamp: messageDetails.timestamp,
-//                 received: messageDetails.received,
-//                 author: messageDetails.author,
-//                 id:messageDetails._id
-//             }
-//             )
-//         }else {
-//             console.log('Error triggered Pusher');
-//         }
-
-//     })
-
-// })
-
-// pusher.trigger('my-channel', 'my-event', {
-//   'message': 'hello world'
-// });
-
-
-
 app.use(express.json())
 
 app.use(cors())
@@ -84,26 +38,6 @@ app.use(cors())
 
 app.get('/',(req,res)=>res.status(200).send('hello world'))
 
-// app.get('/messages/sync',(req,res)=> {
-//     Messages.find((err,data)=> {
-//         if (err) {
-//             res.status(500).send(err)
-//         }else{
-//             res.status(200).send(data)
-//         }
-//     })
-// })
-
-// app.post('/messages/new',(req,res)=> {
-//     const dbMessage = req.body
-//     Messages.create(dbMessage, (err,data)=> {
-//         if (err) {
-//             res.status(500).send(err)
-//         }else{
-//             res.status(201).send(`new message created: \n ${data}`)
-//         }
-//     })
-// })
 
 app.use('/rooms',roomRouter);
 // app.use('/messages',messageRouter);
